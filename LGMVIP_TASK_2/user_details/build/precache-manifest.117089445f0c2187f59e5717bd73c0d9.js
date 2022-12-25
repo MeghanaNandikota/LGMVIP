f5c161c100446b0478af1fababfdd35d30e5e700
meghana <104339280+MeghanaNandikota@users.noreply.github.com>
2022-12-25 22:17:49 +0530

    Add files via upload

---
+++ LGMVIP_TASK_2/user_details/build/precache-manifest.117089445f0c2187f59e5717bd73c0d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "02ec08433124cb1c4c01",
+    "url": "/static/css/main.63533a33.chunk.css"
+  },
+  {
+    "revision": "02ec08433124cb1c4c01",
+    "url": "/static/js/main.02ec0843.chunk.js"
+  },
+  {
+    "revision": "628cdbe65f8cad0e111f",
+    "url": "/static/js/1.628cdbe6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "35fe5991a738875b127c590866e4935a",
+    "url": "/index.html"
+  }
+];
